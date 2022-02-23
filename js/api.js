199bb7e760123fbdd55195bfb509788ec0e080f1
partho2589 <parthomalo615@.com>
2022-02-23 17:58:18 +0600

    add api file

--- js/api.js
+++ js/api.js
@@ -10,3 +10,25 @@ function loadData() {
     .then(response =>response.json())
     .then(data => console.log(data))
 }
+
+
+function lodaPosts (){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+function loadUsers (){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => displayUsers(data))
+}
+
+function displayUsers (data){
+  const ul = document.getElementById('users');
+    for( const user of data){
+      const li = document.createElement('li')
+      li.innerText = `name: ${user.name} , email: ${user.email}`
+      ul.appendChild(li);
+    }
+}
